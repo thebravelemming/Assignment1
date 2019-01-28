ab8bddd6d3ecd6925e326e71c57867f55e182f1d
Derek Yohn <d.yohn43@gmail.com>
2019-01-28 14:53:36 -0500

    Add SimpleHammer to WeaponFactory options
    The SimpleHammer is now available. It can be instantiated with
    either "simplehammer" or "SimpleHammer" as the provided string.

--- src/edu/fsu/cs/cen5035/WeaponFactory.java
+++ src/edu/fsu/cs/cen5035/WeaponFactory.java
@@ -8,10 +8,13 @@ public class WeaponFactory {
 
     public static Weapon getWeapon(String type) {
         switch (type) {
-            case "sword":
-                return new Sword();
-            case "spear":
+	    case "sword":
+	        return new Sword();
+	    case "spear":
             	return new Spear();
+	    case "simplehammer":
+	    case "SimpleHammer":
+		return new SimpleHammer();
             default:
                 throw new IllegalArgumentException("Invalid type");
         }
