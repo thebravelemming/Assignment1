3288b0b071b76c48b26374537f6e4169a49a1164
Derek Yohn <d.yohn43@gmail.com>
2019-01-28 21:33:17 -0500

    Fixed indentation issues
    JDEE was acting up, and I had to manually reformat a few lines.

--- src/edu/fsu/cs/cen5035/Spear.java
+++ src/edu/fsu/cs/cen5035/Spear.java
@@ -17,11 +17,11 @@ public class Spear extends BasicWeapon implements Weapon {
 
     @Override
     public int hit(int armor) {
-	int damage = DAMAGE - (int) (0.8 * armor);
-        if (damage < 0) {
-            return 0;
-	}
-	return damage;
+		int damage = DAMAGE - (int) (0.8 * armor);
+		if (damage < 0) {
+			return 0;
+		}
+		return damage;
     }
 
 }
