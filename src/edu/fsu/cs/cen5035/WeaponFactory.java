84a8c89657e3d503b7154c9e9b0c061b1e2a789c
Derek Yohn <d.yohn43@gmail.com>
2019-01-28 19:51:55 -0500

    Revert "Add SimpleHammer to WeaponFactory options"
    This reverts commit ab8bddd6d3ecd6925e326e71c57867f55e182f1d.
    
    A change was mistakenly made to Spear.java. This reversion will
    correct this.

--- src/edu/fsu/cs/cen5035/WeaponFactory.java
+++ src/edu/fsu/cs/cen5035/WeaponFactory.java
@@ -8,13 +8,10 @@ public class WeaponFactory {
 
     public static Weapon getWeapon(String type) {
         switch (type) {
-	    case "sword":
-	        return new Sword();
-	    case "spear":
+            case "sword":
+                return new Sword();
+            case "spear":
             	return new Spear();
-	    case "simplehammer":
-	    case "SimpleHammer":
-		return new SimpleHammer();
             default:
                 throw new IllegalArgumentException("Invalid type");
         }
