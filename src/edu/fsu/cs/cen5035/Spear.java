9011c17c4d435fe0a1da7b35fc319e24d840cee9
Derek Yohn <d.yohn43@gmail.com>
2019-01-28 20:00:53 -0500

    Corrected code to match original
    The previous revert did not fix the file, so it was recoded
    manually to match the original file.

--- src/edu/fsu/cs/cen5035/Spear.java
+++ src/edu/fsu/cs/cen5035/Spear.java
@@ -3,8 +3,9 @@ package edu.fsu.cs.cen5035;
 /**
  * @author Javier
  */
-public class Spear extends BasicWeapon implements Weapon {
 
+public class Spear extends BasicWeapon implements Weapon {
+    
     public Spear() {
         super(40);
     }
@@ -16,11 +17,11 @@ public class Spear extends BasicWeapon implements Weapon {
 
     @Override
     public int hit(int armor) {
-        int damage = DAMAGE - (int) (0.8 * armor);
+	int damage = DAMAGE - (int) (0.8 * armor);
         if (damage < 0) {
             return 0;
-        }
-        return damage;
+	}
+	return damage;
     }
 
 }
