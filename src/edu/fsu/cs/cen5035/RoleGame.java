e94c46c08e89a89ef4923e52d6668cb98fe755f5
Derek Yohn <d.yohn43@gmail.com>
2019-02-01 19:45:25 -0500

    Remove TODO comment line

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -18,7 +18,6 @@ public class RoleGame {
         System.out.println("Spear has " + weapon.hit() + " of damage.");        
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
-        //TODO: Add the remainig weapons here
 		weapon = WeaponFactory.getWeapon("simplehammer");
         System.out.println("SimpleHammer has " + weapon.hit() + " of damage.");        
         System.out.println("SimpleHammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
