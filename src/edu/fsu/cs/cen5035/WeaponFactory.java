d0c5872e4590087745be46ae13ced2798fbd09f3
Derek Yohn <d.yohn43@gmail.com>
2019-02-01 19:17:05 -0500

    Add weapons to factory
    The SimpleHammer, CrazyRandomSword, and LuckyDagger added to the
    WeaponFactory.

--- src/edu/fsu/cs/cen5035/WeaponFactory.java
+++ src/edu/fsu/cs/cen5035/WeaponFactory.java
@@ -8,10 +8,16 @@ public class WeaponFactory {
 
     public static Weapon getWeapon(String type) {
         switch (type) {
-            case "sword":
+		    case "sword":
                 return new Sword();
-            case "spear":
+		    case "spear":
             	return new Spear();
+		    case "simplehammer":
+     			return new SimpleHammer();
+    		case "crazyrandomsword":
+			    return new CrazyRandomSword();
+    		case "luckydagger":
+	    		return new LuckyDagger();
             default:
                 throw new IllegalArgumentException("Invalid type");
         }
