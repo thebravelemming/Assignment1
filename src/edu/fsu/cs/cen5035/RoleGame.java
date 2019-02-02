6261a174f5847d0801eab39d90a5faf9df7a8804
Derek Yohn <d.yohn43@gmail.com>
2019-02-01 19:23:55 -0500

    Add tests for new weapons
    Add basic functionality tests for SimpleHammer, CrazyRandomSword,
    and LuckyDagger.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -19,6 +19,17 @@ public class RoleGame {
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
         //TODO: Add the remainig weapons here
+		weapon = WeaponFactory.getWeapon("simplehammer");
+        System.out.println("SimpleHammer has " + weapon.hit() + " of damage.");        
+        System.out.println("SimpleHammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+		weapon = WeaponFactory.getWeapon("crazyrandomsword");
+        System.out.println("CrazyRandomSword has " + weapon.hit() + " of damage.");        
+        System.out.println("CrazyRandomSword was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+		weapon = WeaponFactory.getWeapon("luckydagger");
+        System.out.println("LuckyDagger has " + weapon.hit() + " of damage.");        
+        System.out.println("LuckyDagger was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
     }
 }
